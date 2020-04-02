16fc217c510c949a6e1fb903a5be18b8c0a57d7c
MartinG <martin.guezen@gmail.com>
2020-04-02 15:36:34 +0200

    finalizing back-end and tests

--- test/helpers.js
+++ test/helpers.js
@@ -1,14 +1,14 @@
 export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 
-export const EVM_REVERT = 'Returned error: VM Exception while processing transaction: revert'
+export const EVM_REVERT = 'VM Exception while processing transaction: revert'
 
 export const ether = (n) => {
-	return new web3.utils.BN(
-	web3.utils.toWei(n.toString(),'ether')
-	)
+  return new web3.utils.BN(
+    web3.utils.toWei(n.toString(), 'ether')
+  )
 }
-//same as ether
-export const tokens = (n) => ether(n)
 
+// Same as ether
+export const tokens = (n) => ether(n)
 
 
